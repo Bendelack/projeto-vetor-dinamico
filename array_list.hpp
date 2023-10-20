5f40bba3685d28f09cf6d4a6bdf216766b8fc25c
Bendelack <rickk1010@gmail.com>
2023-10-19 22:26:07 -0300

    inicio do metodo pushfront

--- array_list.hpp
+++ array_list.hpp
@@ -3,55 +3,67 @@
 
 
 class array_list {
-private:
-    int* data;
-    unsigned int size_, capacity_;
-    void increase_capacity() {}
-public:
-    array_list() {
-        data = new int[8];
-        this->size_ = 0;
-        this->capacity_ = 8;
-    }
-    ~array_list() {
-        delete[] data;
-    }
-    unsigned int size() {
-        return this->size_;
-    }
-    unsigned int capacity() {
-        return this->capacity_;
-    }
-    double percent_occupied() {}
-    bool insert_at(unsigned int index, int value) {}
-    bool remove_at(unsigned int index) {
-        if (index >= this->size_)
-            return false; // Não removeu
-        for (int i = index + 1; i < this->size_; ++i) {
-            this->data[i - 1] = this->data[i];
+    private:
+        int* data;
+        unsigned int size_, capacity_;
+        void increase_capacity() {}
+    public:
+        array_list() {
+            data = new int[8];
+            this->size_ = 0;
+            this->capacity_ = 8;
         }
-        this->size_--;
-        return true; // Removeu
-    }
-    int get_at(unsigned int index) {
-        // TODO: Check if index is valid
-        return this->data[index];
-    }
-    void clear() {}
-    void push_back(int value) {
-        if (this->size_ == this->capacity_)
-            increase_capacity();
-        this->data[size_++] = value;
-    }
-    void push_front(int value) {}
-    bool pop_back() {}
-    bool pop_front() {}
-    int front(){}
-    int back(){}
-    bool remove(int value) {}
-    int find(int value) {}
-    int count(int value) {}
-    int sum() {}
+        ~array_list() {
+            delete[] data;
+        }
+        unsigned int size() {
+            return this->size_;
+        }
+        unsigned int capacity() {
+            return this->capacity_;
+        }
+        double percent_occupied() {}
+        bool insert_at(unsigned int index, int value) {
+            
+        }
+        bool remove_at(unsigned int index) {
+            if (index >= this->size_)
+                return false; // Não removeu
+            for (int i = index + 1; i < this->size_; ++i) {
+                this->data[i - 1] = this->data[i];
+            }
+            this->size_--;
+            return true; // Removeu
+        }
+        int get_at(unsigned int index) {
+            // TODO: Check if index is valid
+            return this->data[index];
+        }
+        void clear() {}
+        void push_back(int value) {
+            if (this->size_ == this->capacity_)
+                increase_capacity();
+            this->data[size_++] = value;
+        }
+        void push_front(int value) {
+            if ( this->size_ == this->capacity_ )
+                increase_capacity();
+            array_list new_data;// = new array_list();
+            new_data.insert_at(0, value);
+            int j = 0;
+            for (int i = 1; i < this->size_; i++){
+                new_data.insert_at(i, data[j]);
+                j++;
+            }
+        }
+        bool pop_back() {}
+        bool pop_front() {}
+        int front(){}
+        int back(){}
+        bool remove(int value) {}
+        int find(int value) {}
+        int count(int value) {}
+        int sum() {}
 };
 
 
