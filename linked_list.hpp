3a2cc248e397639d93fb7e0efc1c893e93dc52b3
RICK HILL BENDELACK DE MOURA <20231014040026@ifrn.local>
2023-10-24 08:23:18 -0300

    incio da lista duplamente ligada

--- linked_list.hpp
+++ linked_list.hpp
@@ -24,10 +24,38 @@ public:
             delete to_remove;
         }
     }
-    unsigned int size() {}
+    unsigned int size() { // O(n), onde n é a quantidade de elementos.
+        return this->size_;
+    }
     unsigned int capacitty() {}
     double percent_occupied() {}
-    bool insert_at(unsigned int index, int value) {}
+    bool insert_at(unsigned int index, int value) {
+        if ( index == 0 ){
+            this->push_front(value);
+            return true;
+        }
+
+        if ( index >= this->size_ ){
+            this->push_back(value);
+            return true;
+        }
+
+        int_node* new_node = new int_node;
+
+        int_node* current = this->head;
+
+        for ( unsigned int i = 0; i < index; i++ )
+            current = current->next;
+
+        new_node->value = value;
+        new_node->next = current;
+        new_node->prev = current->prev;
+        current->prev = new_node;
+        this->size_++;
+        return true;
+
+    }
+
     bool remove_at(unsigned int index) {
         if (index >= this->size_)
             return false; // Não removeu
@@ -39,9 +67,21 @@ public:
         if (to_remove->next != nullptr)
             to_remove->next->prev = to_remove->prev;
         delete to_remove;
+        this->size_--;
         return true; // Removeu
     }
-    int get_at(unsigned int index) {}
+
+    int get_at(unsigned int index) {
+        if (index >= this->size_)
+            return -1;
+        int_node* current = this->head;
+
+        for ( unsigned int i = 0; i < index; i++ )
+            current = current->next;
+
+        return current->value;
+        
+    }
     void clear() {}
     void push_back(int value) {}
     void push_front(int value) {
@@ -54,15 +94,59 @@ public:
         else
             this->head->prev = new_node;
         this->head = new_node;
+        this->size_++;
     }
     bool pop_back() {}
     bool pop_front() {}
-    int front() {}
-    int back() {}
+
+    int front() { // O(1)
+        if ( this->size_ > 0 )
+            return this->head->value;
+        return -1;
+    }
+
+    int back() { // O(1)
+        if ( this->size_ > 0 )
+            return this->tail->value;
+        return -1;
+    }
+
     bool remove(int value) {}
-    int find(int value) {}
-    int count(int value) {}
-    int sum() {}
+
+    int find(int value) {
+        int_node* current = this->head;
+        int i = 0;
+        while ( current != nullptr ){
+            if ( current->value == value )
+                return i;
+            current = current->next;
+        }
+
+        return -1;
+    }
+
+    int count(int value) {
+        int_node* current = this->head;
+        int c = 0;
+        while ( current != nullptr ){
+            if ( current->value == value )
+                c++;
+            current = current->next;
+        }
+
+        return c;
+    }
+
+    int sum() { // O(n), onde n é a quantidade de elementos no vetor
+        int_node* current = this->head;
+        int sumValues = 0;
+        while ( current != nullptr ){
+            sumValues += current->value;
+            current = current->next;
+        }
+
+        return sumValues;
+    }
 };
 
 #endif // __LINKED_LIST_IFRN__
